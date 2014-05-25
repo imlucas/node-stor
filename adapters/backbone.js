9e8c8acd2cadd9bbdbdf8837459d55b21fcd4885
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-05-25 13:37:38 -0400

    damn backbone scope malarchy...

--- adapters/backbone.js
+++ adapters/backbone.js
@@ -1,54 +1,52 @@
 function BackboneAdapter(store, ns){
   store.ns = ns  + '~';
-  this.store = store;
-  this.ns = ns;
+  // normalize the api taxonomy with a convenience that returns the
+  // correct function for a store.
+  function getHandler(method){
+    var taxonomy = {
+        create: store.set,
+        read: store.get,
+        update: store.set,
+        'delete': store.remove,
+        patch: store.set,
+        findAll: store.all,
+        findById: store.get
+      };
+    return store[method] || taxonomy[method];
+  }
+  function sync(method, model, options){
+    var args = [], store = store;
+
+    if(Array.isArray(model.models)){
+      // it's a collection, but backbone sends the same methods as models.
+      method = 'all';
+    }
+    else {
+      if(method === 'create' && !model.id){
+        return store.id(function(err, id){
+          model.id = id;
+          model.set(model.idAttribute, id);
+          sync(method, model, options);
+        });
+      }
+      args.push(model.id);
+
+      if(['create', 'update', 'patch'].indexOf(method) > -1){
+        args.push(model.toJSON());
+      }
+    }
+    args.push(function(err, data){
+      if(err) return options.error(err);
+      options.success(data);
+    });
+    var handler = getHandler(method);
+    if(!handler){
+      throw new Error('No handler for ' + method + ' on ' + store.name);
+    }
+    handler.apply(handler, args);
+  }
+  return {sync: sync, store: store, ns: ns};
 }
-// normalize the api taxonomy with a convenience that returns the
-// correct function for a store.
-BackboneAdapter.prototype.getHandler = function(method){
-  var taxonomy = {
-      create: this.store.set,
-      read: this.store.get,
-      update: this.store.set,
-      'delete': this.store.remove,
-      patch: this.store.set,
-      findAll: this.store.all,
-      findById: this.store.get
-    };
-  return this.store[method] || taxonomy[method];
-};
-
-BackboneAdapter.prototype.sync = function(method, model, options){
-  var args = [], self = this, store = this.store;
-
-  if(Array.isArray(model.models)){
-    // it's a collection, but backbone sends the same methods as models.
-    method = 'all';
-  }
-  else {
-    if(method === 'create' && !model.id){
-      return store.id(function(err, id){
-        model.id = id;
-        model.set(model.idAttribute, id);
-        self.sync(method, model, options);
-      });
-    }
-    args.push(model.id);
-
-    if(['create', 'update', 'patch'].indexOf(method) > -1){
-      args.push(model.toJSON());
-    }
-  }
-  args.push(function(err, data){
-    if(err) return options.error(err);
-    options.success(data);
-  });
-  var handler = this.getHandler(method);
-  if(!handler){
-    throw new Error('No handler for ' + method + ' on ' + this.store.name);
-  }
-  handler.apply(this, args);
-};
 
 module.exports = function(store, ns){
   return new BackboneAdapter(store, ns);
